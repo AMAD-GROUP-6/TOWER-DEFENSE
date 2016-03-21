9dc8a9ab39d0fec437ac0f8384db7ffea7dd6ace
Josh Winter <winterjoshr@gmail.com>
2016-03-20 21:25:45 -0400

    added some options to settings menu

--- app/src/main/java/csc415/towerdefense/OptionsActivity.java
+++ app/src/main/java/csc415/towerdefense/OptionsActivity.java
@@ -5,15 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class OptionsActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+public class OptionsActivity extends Activity implements SeekBar.OnSeekBarChangeListener, CompoundButton.OnCheckedChangeListener {
 
     SeekBar volume;
     TextView txtVolume;
+    Switch musicEnabledSwitch;
+    Switch soundEnabledSwitch;
+    Switch hideNavBar; // for devices that don't have hardware nav buttons (home, back, recents), they have on screen buttons
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +32,24 @@ public class OptionsActivity extends Activity implements SeekBar.OnSeekBarChange
         volume = (SeekBar) findViewById(R.id.seekBarVolume);
         volume.setOnSeekBarChangeListener(this);
 
+        musicEnabledSwitch = (Switch) findViewById(R.id.music_enabled_switch);
+        soundEnabledSwitch = (Switch) findViewById(R.id.sound_effects_enabled_switch);
+        hideNavBar = (Switch) findViewById(R.id.hide_nav_switch);
+
+        musicEnabledSwitch.setOnCheckedChangeListener(this);
+        soundEnabledSwitch.setOnCheckedChangeListener(this);
+
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton s, boolean checked) {
+       /* if(s == this.musicEnabledSwitch){
 
+        }
+        else if (s == this.soundEnabledSwitch) {
+
+        }*/
+    }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
